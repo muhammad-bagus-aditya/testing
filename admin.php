11c18d63c85831f9133703845e248b265708ac18
muhammad-bagus-aditya <bagusaditya9a19.dev@gmail.com>
2025-04-11 13:08:27 +0700

    fixing bug

--- admin.php
+++ admin.php
@@ -30,7 +30,7 @@ while ($row = $res->fetch_assoc()) {
 <body>
   <nav class="navbar navbar-expand-lg ">
     <div class="container">
-      <a href="/" class="navbar-brand">makaNNES</a>
+      <a href="index.php" class="navbar-brand">makaNNES</a>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
--- edit.php
+++ edit.php
@@ -26,7 +26,7 @@ $menu = $res->fetch_assoc();
 <body>
   <nav class="navbar navbar-expand-lg ">
     <div class="container">
-      <a href="/" class="navbar-brand">makaNNES</a>
+      <a href="index.php" class="navbar-brand">makaNNES</a>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
