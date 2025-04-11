e3aff753e56a0516238e812557212c2998236518
muhammad-bagus-aditya <bagusaditya9a19.dev@gmail.com>
2025-04-11 13:11:18 +0700

    fixing bug

--- edit-post.php
+++ edit-post.php
@@ -2,6 +2,6 @@
 
 $conn = mysqli_connect("localhost", "root", "", "makannes");
 
-$res = mysqli_query($conn, 'UPDATE menu SET  name="' . $_POST['name'] . '", price="' . $_POST['price'] . '", image="' . $_POST['image'] . '" WHERE id="' . $_POST['id'] . '"');
+$res = mysqli_query($conn, 'UPDATE menu SET  name="' . $_POST['name'] . '", price="' . $_POST['price'] . '", location="' . $_POST['location'] . '", image="' . $_POST['image'] . '" WHERE id="' . $_POST['id'] . '"');
 
 header('location: admin.php');
--- tambah-post.php
+++ tambah-post.php
@@ -2,6 +2,6 @@
 
 $conn = mysqli_connect("localhost", "root", "", "makannes");
 
-$res = mysqli_query($conn, 'INSERT INTO menu (id, name, price, image) VALUES ("", "' . $_POST['name'] . '", "' . $_POST['price'] . '", "' . $_POST['image'] . '")');
+$res = mysqli_query($conn, 'INSERT INTO menu (id, name, price, location, image) VALUES ("", "' . $_POST['name'] . '", "' . $_POST['price'] . '", "' . $_POST['location'] . '", "' . $_POST['image'] . '")');
 
 header('location: admin.php');
